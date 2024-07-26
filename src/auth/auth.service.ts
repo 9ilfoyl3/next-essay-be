76acf743073bfc71a59d8eea157355b49c4976c8
chengjm <3497346788@qq.com>
2024-07-26 17:04:52 +0800

    feat: add base login and register function

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,13 +1,47 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 import { LoginDto } from './dto/login.dto';
 import { RegisterDto } from './dto/register.dto';
+import { hash, verify } from 'argon2';
 
 @Injectable()
 export class AuthService {
-  login(loginDto: LoginDto) {
-    return loginDto;
+  constructor(private readonly prisma: PrismaService) {}
+  async login(loginDto: LoginDto) {
+    const user = await this.prisma.user.findFirst({
+      where: {
+        phone: loginDto.phone,
+      },
+    });
+    if (user) {
+      const isPasswordCorrect = await verify(user.password, loginDto.password);
+      if (isPasswordCorrect) {
+        return user;
+      } else {
+        throw new HttpException('Invalid password', 400);
+      }
+    } else {
+      throw new HttpException('User not found', 400);
+    }
   }
-  register(registerDto: RegisterDto) {
-    return registerDto;
+  async register(registerDto: RegisterDto) {
+    const user = await this.prisma.user.findFirst({
+      where: {
+        phone: registerDto.phone,
+      },
+    });
+
+    if (user) {
+      throw new HttpException('User already exists', 400);
+    }
+
+    const newUser = await this.prisma.user.create({
+      data: {
+        phone: registerDto.phone,
+        password: await hash(registerDto.password),
+      },
+    });
+
+    return newUser;
   }
 }
